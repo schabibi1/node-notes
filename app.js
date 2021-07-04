79d4c3be4ab946dd4ec0e1ba81d6b5ba9716d206
Arisa Fukuzaki <25793187+schabibi1@users.noreply.github.com>
2021-07-04 23:59:16 +0200

    iadd: commit basic note app architecture

---
+++ app.js
@@ -0,0 +1,72 @@
+const chalk = require('chalk')
+const yargs = require('yargs')
+const notes = require('./notes')
+
+yargs.version('1.1.0')
+
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler(argv) {
+    notes.addNote(argv.title, argv.body)
+  }
+})
+
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+  },
+  handler(argv) {
+    notes.removeNote(argv.title)
+  }
+})
+
+yargs.command({
+  command: 'list',
+  describe: 'List a note',
+  builder: {
+    title: {
+      describe: 'Note list',
+      demandOption: false,
+      type: 'string'
+    },
+  },
+  handler(argv) {
+    notes.listNotes(argv.title)
+  }
+})
+
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: 'string'
+    },
+  },
+  handler(argv) {
+    notes.readNote(argv.title)
+  }
+})
+
+yargs.parse()
